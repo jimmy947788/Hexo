97184f0426cf2cee7217b18584e5462937056fb1
John Wu <me@johnwu.cc>
2017-04-09 22:34:54 +0800

    initial version

---
+++ source/_posts/hello-word.md
@@ -0,0 +1,5 @@
+title: hello word
+author: Jimmy Wu
+date: 2017-04-09 22:30:00
+---
+## hello-word
---
+++ themes/next/source/js/src/custom.js
@@ -0,0 +1,38 @@
+﻿$(function () {
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyC2MzU9mQj7ym2JPcLMN9uMLo1i0YrEFTQ",
+		authDomain: "blog-jimmywu-cc.firebaseapp.com",
+		databaseURL: "https://blog-jimmywu-cc.firebaseio.com",
+		projectId: "blog-jimmywu-cc",
+		storageBucket: "blog-jimmywu-cc.appspot.com",
+		messagingSenderId: "383220126864"
+    };
+    firebase.initializeApp(config);
+    var database = firebase.database();
+
+    function changeUrlToKey(url) {
+        return url.replace(new RegExp('\\/|\\.', 'g'), "_");
+    }
+
+    function readVisits(selector, url, isReadOnly) {
+        var db_key = changeUrlToKey(window.location.host) + "/" + changeUrlToKey(url);
+        database.ref(db_key).once("value").then(function (result) {
+            var count = parseInt(result.val() || 0);
+            if (!isReadOnly) {
+                count += 1;
+                database.ref(db_key).set(count);
+            }
+            if (selector.length > 0) {
+                selector.html(count);
+            };
+        });
+    }
+    readVisits($("#visits .count"), "/");
+
+    $(".pageviews").each(function () {
+        var postUrl = $(this).data("path");
+        var isReadOnly = window.location.pathname === "/";
+        readVisits($(this).find(".count"), postUrl, isReadOnly);
+    });
+});
